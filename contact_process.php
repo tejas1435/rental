7048a41492db9fae3b7c8c277a8c4f29d25e048f
tejas7 <tejas@goldenmace.com>
2020-06-05 23:29:54 +0530

    contact form email set

--- contact_process.php
+++ contact_process.php
@@ -1,10 +1,9 @@
 <?php
 
-    $to = "rockybd1995@gmail.com";
+    $to = "tejaspatel9736@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
-    $number = $_REQUEST['number'];
     $cmessage = $_REQUEST['message'];
 
     $headers = "From: $from";
@@ -13,24 +12,40 @@
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $subject = "You have a message from your Bitmap Photography.";
-
-    $logo = 'img/logo.png';
-    $link = '#';
-
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
-	$body .= "<table style='width: 100%;'>";
-	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
-	$body .= "</td></tr></thead><tbody><tr>";
-	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
-	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
-	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
-	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
-	$body .= "</tbody></table>";
-	$body .= "</body></html>";
+	$body = '
+	<html xmlns="http://www.w3.org/1999/xhtml">
+		<head>
+			<title>A Simple Responsive HTML Email</title>
+			<style type="text/css">
+			body {margin: 0; padding: 0; min-width: 100%!important;}
+			.content {width: 100%; max-width: 600px;}  
+			</style>
+		</head>
+		<body bgcolor="#f6f8f1">
+			<table width="70%" bgcolor="#f6f8f1" border="0" cellpadding="0" cellspacing="0">
+				<tr>
+					<td>
+						<table class="content" align="center" cellpadding="0" cellspacing="0" border="0">
+							<tr>
+								<td class="text-center">
+									<h1>Online Rent Agreement</h1>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<h4>Name:- '.$name.'</h4>
+									<h4>Email:- '.$from.'</h4>
+									<h4>Subject:- '.$subject.'</h4>
+									<p><b>Message:-</b> '.$cmessage.'</p>
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+			</table>
+		</body>
+	</html>
+			';
 
     $send = mail($to, $subject, $body, $headers);
 
