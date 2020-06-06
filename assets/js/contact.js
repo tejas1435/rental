dc895b9a4a70f3fba2ada9374a3f78d51f2a5780
tejas7 <tejas@goldenmace.com>
2020-06-06 22:55:51 +0530

    contact page some changes

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -59,22 +59,17 @@ $(document).ready(function(){
                     type:"POST",
                     data: $(form).serialize(),
                     url:"contact_process.php",
+                    beforeSend: function() {
+                        $('.button-contactForm').html('Sending....');
+                    },
                     success: function() {
                         $('#contactForm :input').attr('disabled', 'disabled');
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $(this).find(':input').attr('disabled', 'disabled');
-                            $(this).find('label').css('cursor','default');
-                            $('#success').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#success').modal('show');
-                        })
+                        $('#contactForm :input').attr("style", "display: none !important");
+                        alert('Thanks for contacting use. We contact you as soon as possible.');
+                        location.reload();
                     },
                     error: function() {
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $('#error').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#error').modal('show');
-                        })
+                        alert('Please try after sometime.');
                     }
                 })
             }
